5c8da75bb4f8215bcc44659387ea8304f79a2ff7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-02-08 11:02:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ from streamlit_pandas_profiling import st_profile_report
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  dataframe = pd.read_csv(uploaded_file)
+  df = pd.read_csv(uploaded_file)
   
-  dataframe
+  df
 
-  pr = dataframe.profile_report()
+  pr = df.profile_report()
 
   st_profile_report(pr)
 
