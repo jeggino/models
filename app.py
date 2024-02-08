7c521778a7513077c2dce5d7055a1f6341a9bf0c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-02-08 10:44:37 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn import tree
+import numpy as np
+import streamlit as st
+
+uploaded_file = st.file_uploader("Choose a file")
+
+dataframe = pd.read_csv(uploaded_file)
+
+dataframe
