199595001c7098bdaba99a860fa59541b0caa2de
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-02-08 10:53:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,15 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn import tree
 import numpy as np
 import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
 
+
+st_profile_report(pr)
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   dataframe = pd.read_csv(uploaded_file)
   
   dataframe
+
+  pr = df.profile_report()
+
